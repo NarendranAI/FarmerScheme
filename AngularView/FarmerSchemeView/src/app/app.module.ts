ffc491699e08fcfa579cd363622f035e2a00937b
Anushya_Selvan <Anushya.S@lntinfotech.com>
2021-01-09 18:04:29 +0530

    farmer registration

--- AngularView/FarmerSchemeView/src/app/app.module.ts
+++ AngularView/FarmerSchemeView/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,7 +39,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   imports: [
   BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [SQLServerService],
   bootstrap: [AppComponent]
--- AngularView/FarmerSchemeView/src/app/farmer-registration/farmer-registration.component.ts
+++ AngularView/FarmerSchemeView/src/app/farmer-registration/farmer-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-farmer-registration',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./farmer-registration.component.css']
 })
 export class FarmerRegistrationComponent implements OnInit {
-
+ data :{UserId:number;
+UserName:string;
+Email:string;
+MobileNumber:string}
   constructor() { }
-
+ submit(data)
+ {
+   this.data=data;
+   console.log(data);
+ }
   ngOnInit(): void {
   }
 
